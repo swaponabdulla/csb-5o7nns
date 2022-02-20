0469fb7376e6f7aad3a5201959886971254c2999
Code Sandbox <hello@codesandbox.io>
2022-02-20 17:02:07 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3cf3a3cfc9f009ee8bc8e03de35ce720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd45af7f3a8d8b9a69ccdc2aad70ddf",
+    "url": "https://swaponabdulla.github.io/csb-5o7nns/index.html"
+  },
+  {
+    "revision": "bd5e9eac23554db7aa12",
+    "url": "https://swaponabdulla.github.io/csb-5o7nns/static/css/2.c0e4f2a0.chunk.css"
+  },
+  {
+    "revision": "b61502d96f5d3c771857",
+    "url": "https://swaponabdulla.github.io/csb-5o7nns/static/css/main.78aed21a.chunk.css"
+  },
+  {
+    "revision": "bd5e9eac23554db7aa12",
+    "url": "https://swaponabdulla.github.io/csb-5o7nns/static/js/2.476beaf0.chunk.js"
+  },
+  {
+    "revision": "b61502d96f5d3c771857",
+    "url": "https://swaponabdulla.github.io/csb-5o7nns/static/js/main.21e74b43.chunk.js"
+  },
+  {
+    "revision": "f8415d4cc2a377d5e119",
+    "url": "https://swaponabdulla.github.io/csb-5o7nns/static/js/runtime~main.abe26475.js"
+  }
+]);
